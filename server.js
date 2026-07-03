e036356ab6f32d5332e483a77393e140a49b5a68
heavenly brownie <spiritdude484@gmail.com>
2026-07-03 15:38:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: { origin: "*" }
+    cors: {
+        origin: "*"
+    }
 });
 
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+    console.log("User connected");
 
     socket.on("chat-message", (msg) => {
         io.emit("chat-message", msg);
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 650,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
     });
 });
+    win.loadFile(path.join(__dirname, "login.html")); //START HERE
 
+// IMPORTANT: needed for hosting platforms
 const PORT = process.env.PORT || 3000;
+    win.setMenuBarVisibility(false);
+}
 
 server.listen(PORT, () => {
-    console.log("Server running on port", PORT);
+    console.log("Server running on port " + PORT);
 });
+app.whenReady().then(createWindow);
